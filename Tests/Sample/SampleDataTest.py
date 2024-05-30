07c2df684d03cdde45b3c4a61913c77ed0e272b6
kalus <kaluski1212@wp.pl>
2024-05-30 19:13:00 +0200

    Refactor code

--- Tests/Sample/SampleDataTest.py
+++ Tests/Sample/SampleDataTest.py
@@ -1,14 +1,21 @@
 import sys
 import os
-from Tests.Common import Common
 
-UsageInfo = "Usage: python SampleDataTests.py <quantile[0,1]>"
+
+parent_directory = os.path.abspath('../../')
+sys.path.append(parent_directory)
+
+
+from Tests.Common.Common import ProcessCsvAndFindScanners
+
+
+UsageInfo = "Usage: python SampleDataTests.py <quantile(0,1)>"
 
 
 def run(threshold_quantile):
     script_dir = os.path.dirname(os.path.abspath(__file__))
     file_path = os.path.join(script_dir, "data.csv")
-    Common.ProcessCsvAndFindScanners(file_path, ';', 1, 2, "192.168.1.0/24", threshold_quantile)
+    ProcessCsvAndFindScanners(file_path, ';', 1, 2, "192.168.1.0/24", threshold_quantile)
 
 
 if __name__ == '__main__':
